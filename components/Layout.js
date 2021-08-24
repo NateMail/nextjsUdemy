411d1e1675b3f2cecdda43f2048689d063f19ae8
Nathan Mailhiot <nathanjm12@outlook.com>
2021-08-24 12:38:31 -0400

    Darkmode

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,57 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Head from "next/head";
 import NextLink from "next/link";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
 import {
   AppBar,
   Container,
   Toolbar,
   Typography,
   Link,
+  CssBaseline,
+  Switch,
 } from "@material-ui/core";
 import useStyles from "../utils/styles";
+import { Store } from "../utils/Store";
+import Cookies from "js-cookie";
 
 export default function Layout({ title, children, description }) {
+  const { state, dispatch } = useContext(Store);
+  const { darkMode } = state;
+  const [darkModeState, setDarkModeState] = useState(false);
+  useEffect(() => {
+    setDarkModeState(darkMode);
+  }, [darkMode]);
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? "DARK_MODE_OFF" : "DARK_MODE_ON" });
+    const newDarkMode = !darkMode;
+    setDarkModeState(newDarkMode);
+    Cookies.set("darkMode", newDarkMode ? "ON" : "OFF");
+  };
+
+  const theme = createTheme({
+    typeography: {
+      h1: {
+        fontSize: "1.6rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      h2: {
+        fontSize: "1.4 rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+    },
+    palette: {
+      type: darkMode ? "dark" : "light",
+      primary: {
+        main: "#f0c000",
+      },
+      secondary: {
+        main: "#208080",
+      },
+    },
+  });
   const classes = useStyles();
   return (
     <div>
@@ -18,29 +59,36 @@ export default function Layout({ title, children, description }) {
         <title>{title ? `${title} - Next Amazona` : "Next Amazona"}</title>
         {description && <meta name="description" content={description}></meta>}
       </Head>
-      <AppBar className={classes.navbar} position="static">
-        <Toolbar>
-          <NextLink href="/" passHref>
-            <Link>
-              <Typography className={classes.brand}>amazona</Typography>
-            </Link>
-          </NextLink>
-          <div className={classes.grow}></div>
-          <div>
-            <NextLink href="/cart" passHref>
-              <Link>Cart</Link>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar className={classes.navbar} position="static">
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={classes.brand}>amazona</Typography>
+              </Link>
             </NextLink>
-            <NextLink href="/login" passHref>
-              <Link>Login</Link>
-            </NextLink>
-          </div>
-        </Toolbar>
-      </AppBar>
+            <div className={classes.grow}></div>
+            <div>
+              <Switch
+                checked={darkModeState}
+                onChange={darkModeChangeHandler}
+              ></Switch>
+              <NextLink href="/cart" passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
 
-      <Container className={classes.main}>{children}</Container>
-      <footer className={classes.footer}>
-        <Typography>All rights reserved. Next Amazona</Typography>
-      </footer>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography>All rights reserved. Next Amazona</Typography>
+        </footer>
+      </ThemeProvider>
     </div>
   );
 }
